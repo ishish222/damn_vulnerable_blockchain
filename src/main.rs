99bb85f93ed545ff5dcb604648997f206755682b
ishish.io <ts@ishish.io>
2024-05-31 21:19:05 +0200

    Simple ping between two nodes

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use futures::prelude::*;
+
+use libp2p::{
+    ping,
+    Multiaddr,
+    swarm::SwarmEvent
+};
+use std::{
+    error::Error,
+    time::Duration
+};
+use tracing_subscriber::EnvFilter;
+
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_async_std()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::tls::Config::new,
+            libp2p::yamux::Config::default
+        )?
+        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30)))
+        .build();
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    if let Some(addr) = std::env::args().nth(1) {
+        let remote: Multiaddr = addr.parse()?;
+        swarm.dial(remote)?;
+        println!("Dialed {addr}")
+    }
+
+    loop {
+        match swarm.select_next_some().await {
+            SwarmEvent::NewListenAddr {address, ..} => println!("Listening on {address}"),
+            SwarmEvent::Behaviour(event) => println!("{event:?}"),
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
