7b60e7bf3cf41696c56be04ec1eefdf0ece5eddc
ishish.io <ts@ishish.io>
2024-06-01 21:06:19 +0200

    Partially working, but need to debug

--- src/ishishnet.rs
+++ src/ishishnet.rs
@@ -1,16 +1,30 @@
-use std::convert::TryFrom;
-use std::error::Error;
-use std::fmt::Display;
-use std::fmt::Formatter;
-use std::str;
-use std::str::Utf8Error;
+use std::{
+    convert::TryFrom,
+    error::Error,
+    fmt::{
+        Display,
+        Formatter
+    },
+    str::{
+        self,
+        Utf8Error
+    }
+};
+
+use sha2::{Sha256, Digest};
+use serde::{Serialize, Deserialize};
+
 
 #[derive(Debug)]
 pub enum IshIshError {
     ParseError,
     InvalidMessageHeader,
     EmptyMessage,
-    InvalidEvent
+    InvalidEvent,
+    MiningError,
+    HashConversionFailed,
+    InvalidProofOfWork,
+    PrevHashMismatch
 }
 
 impl Display for IshIshError {
@@ -29,6 +43,12 @@ impl From<Utf8Error> for IshIshError {
     }
 }
 
+impl From<serde_json::Error> for IshIshError {
+    fn from(_: serde_json::Error) -> Self {
+        IshIshError::ParseError
+    }
+}
+
 pub enum IshIshBlockchainEvent<'a> {
     NewBlockMined(&'a str),
     SthElse((&'a str, &'a str))
@@ -40,9 +60,11 @@ impl<'a> TryFrom<&'a Vec<u8>> for IshIshBlockchainEvent<'a> {
     fn try_from(value: &'a Vec<u8>) -> Result<Self, IshIshError> 
     {
         let value_str = str::from_utf8(value)?;
-        
-        let (header, value_str) = explode(value_str).ok_or(IshIshError::InvalidMessageHeader)?;
-        let (message, _) = explode(value_str).ok_or(IshIshError::EmptyMessage)?;
+    
+        let (header, message) = (&value_str[..3], &value_str[3..]);
+
+        //let (header, value_str) = explode(value_str).ok_or(IshIshError::InvalidMessageHeader)?;
+        //let (message, _) = explode(value_str).ok_or(IshIshError::EmptyMessage)?; //needs corrections
 
         match header {
             "NBM" => {
@@ -59,7 +81,155 @@ fn explode(message: &str) -> Option<(&str, &str)> {
             Some((&message[..i], &message[i+1..]))
         }
         else {
-            None
+            Some((&message[..], ""))
         }
     })
+}
+
+
+pub async fn proof_of_work(
+    mut block: IshIshBlock, 
+    difficulty: usize
+    ) -> Result<IshIshBlock, IshIshError> {
+    println!("proof_of_work::start");
+    let mut nonce = 0;
+    loop {
+        let mut hasher = Sha256::new();
+        block.header.nonce = nonce;
+
+        let data = serde_json::to_string(&block)?;
+
+        hasher.update(data);
+
+        let hash: [u8; 32] = match hasher.finalize().try_into() {
+            Ok(arr) => arr,
+            Err(_) => return Err(IshIshError::HashConversionFailed), 
+        };
+
+        if hash.iter().take(difficulty).all(|&b| b == 0) {
+            block.header.cur_hash = hash;
+            println!("proof_of_work::finish");
+            return Ok(block);
+        }
+        nonce += 1;
+        //print!(".");
+    }
+}
+
+fn validate_pow(mut block: IshIshBlock, difficulty: usize) -> Result<bool, IshIshError> {
+    let mut hasher = Sha256::new();
+
+    block.header.cur_hash = [0; 32];
+
+    let data = serde_json::to_string(&block)?;
+    hasher.update(data);
+
+    let hash: [u8; 32] = match hasher.finalize().try_into() {
+        Ok(arr) => arr,
+        Err(_) => return Err(IshIshError::HashConversionFailed), 
+    };
+
+    if hash.iter().take(difficulty).all(|&b| b == 0) {
+        block.header.cur_hash = hash;
+        Ok(true)
+    }
+    else {
+        Ok(false)
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IshIshBlockHeader {
+    nonce: u64,
+    pub cur_hash: [u8; 32],
+    prev_hash: [u8; 32],
+}
+
+impl IshIshBlockHeader {
+    pub fn empty() -> Self {
+        Self {
+            nonce: 0,
+            cur_hash: [0; 32],
+            prev_hash: [0; 32]
+        }
+    }
+
+    pub fn from_prev_hash(prev_hash: [u8; 32]) -> Self {
+        Self {
+            nonce: 0,
+            cur_hash: [0; 32],
+            prev_hash: prev_hash
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IshIshBlock {
+    pub header: IshIshBlockHeader,
+    content: String,
+}
+
+impl IshIshBlock {
+    pub fn empty_from_content(content: String) -> Self {
+        Self {
+            header: IshIshBlockHeader::empty(),
+            content: content
+        }
+    }
+
+    pub fn linked_from_content(content: String, prev_hash: [u8; 32]) -> Self {
+        Self {
+            header: IshIshBlockHeader::from_prev_hash(prev_hash),
+            content: content
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct IshIshBlockchain {
+    pub blocks: Vec<IshIshBlock>,
+}
+
+impl IshIshBlockchain {
+    pub fn new() -> Self {
+        Self {
+            blocks: Vec::new()
+        }
+    }
+
+    pub fn append(&mut self, mut block: IshIshBlock, difficulty: usize) -> Result<(), IshIshError> {
+        self.verify_block(block.clone(), difficulty)?;
+        self.blocks.push(block);
+        Ok(())
+    }
+
+    fn verify_block(&self, block: IshIshBlock, difficulty: usize) -> Result<(), IshIshError> {
+        let pow_ok = validate_pow(block.clone(), difficulty)?;
+        
+        // check POW
+        if !pow_ok {
+            return Err(IshIshError::InvalidProofOfWork);
+        }
+
+        // check link to previous block
+        match self.blocks.last() {
+            Some(last_block) => {
+                if block.header.prev_hash != last_block.header.cur_hash
+                {
+                    return Err(IshIshError::PrevHashMismatch);
+                }
+            },
+            None => {
+                // first block, always good
+            }
+        }
+        
+        Ok(())
+    }
+
+    pub fn verify_chain(&self) -> Result<(), IshIshError> {
+        // For now I assume its good xD
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,19 @@ use tokio::{
 };
 use tracing_subscriber::EnvFilter;
 
+use tokio::sync::mpsc;
+use tokio::sync::watch;
+
 mod ishishnet;
 
-use ishishnet::IshIshBlockchainEvent;
+use ishishnet::{
+    IshIshBlockchainEvent,
+    IshIshBlockchain,
+    proof_of_work,
+    IshIshBlock,
+};
+
+const DEFAULT_DIFFICULTY : usize = 3;
 
 // We create a custom network behaviour that combines Gossipsub and Mdns.
 #[derive(NetworkBehaviour)]
@@ -33,11 +43,52 @@ struct IshIshClientBehavior {
     mdns: mdns::tokio::Behaviour,
 }
 
+// routine for mining a block
+
+async fn mining_task(
+    mut rx: mpsc::Receiver<IshIshBlock>, 
+    mut tx: mpsc::Sender<IshIshBlock>, 
+    mut control_rx: watch::Receiver<bool>, 
+    difficulty: usize) 
+    {
+
+    control_rx.changed().await.unwrap();
+
+    while let Some(block) = rx.recv().await {
+        println!("mining_task: Received block proposition {block:?}");
+
+        tokio::select! {
+            _ = control_rx.changed() => {
+                println!("Received stop signal, terminating mining task.");
+                continue;
+            }
+            mined_block = async {
+                proof_of_work(block, difficulty).await
+            } => {
+                match mined_block {
+                    Ok(mined_block) => {
+                        println!("mining_task: Mined block");
+                        if tx.send(mined_block).await.is_err() {
+                            eprintln!("Failed to send mined block");
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to mine block: {}", e);
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let _ = tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .try_init();
+
+    /* Setting up the data availability layer */
 
     let mut swarm = libp2p::SwarmBuilder::with_new_identity()
         .with_tokio()
@@ -81,23 +132,87 @@ async fn main() -> Result<(), Box<dyn Error>> {
     swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
     // Read full lines from stdin
+    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
     let mut stdin = io::BufReader::new(io::stdin()).lines();
 
     // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
-    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+
+    /* Local blockchain */
+    let mut my_blockchain = IshIshBlockchain::new();
+
+    println!("Starting the local mining thread");
+    let (block_tx, block_rx) = mpsc::channel(10);
+    let (mined_block_tx, mut mined_block_rx) = mpsc::channel(10);
+    let (control_tx, control_rx) = watch::channel(false);
+
+    let difficulty: usize = match std::env::args().nth(1)
+    {
+        Some(v) => v.parse::<usize>().unwrap(),
+        None => DEFAULT_DIFFICULTY as usize
+    };
+    
+    tokio::spawn(mining_task(
+        block_rx, 
+        mined_block_tx, 
+        control_rx, 
+        difficulty));
+
+    /* Request mining of the first block */
+    let first = IshIshBlock::empty_from_content("First".into());
+    block_tx.send(first).await?;
 
     // Kick it off
     loop {
         select! {
             Ok(Some(line)) = stdin.next_line() => {
+                /* 
                 if let Err(e) = swarm
                     .behaviour_mut().gossipsub
                     .publish(topic.clone(), line.as_bytes()) {
                     println!("Publish error: {e:?}");
+                }*/
+                match line.as_str() {
+                    "start" => {
+                        println!("Starting the local mining thread");
+                        control_tx.send(true).unwrap();
+                    },
+                    _ => {
+                        println!("Unknown command: {line}");
+                    }
                 }
-            }
+            },
+            Some(mined_block) = mined_block_rx.recv() => {
+                println!("Successfuly mined block: {:?}", mined_block);
+
+                /* Add the new block to my_blockchain */
+                if let Err(e) = my_blockchain.append(mined_block.clone(), difficulty) {
+                    println!("Append error: {e:?}");
+                }
+
+                /* Send info about the new blockchain */
+                let mut line = String::from("NBM");
+                let blockchain_serialized = serde_json::to_string(&my_blockchain)?;
+                line.push_str(&blockchain_serialized);
+
+                println!("Sending line: {line:?}");
+                
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line.as_bytes()) {
+                        println!("Publish error: {e:?}");
+                    }
+
+                // Requestng mining new block
+                let new_content = format!("Block number: {}", my_blockchain.blocks.len());
+                let mut next = IshIshBlock::linked_from_content(
+                    new_content, 
+                    mined_block.header.cur_hash
+                );
+                block_tx.send(next).await?;
+
+            },
             event = swarm.select_next_some() => match event {
                 SwarmEvent::Behaviour(IshIshClientBehaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
                     for (peer_id, _multiaddr) in list {
@@ -117,8 +232,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     message,
                 })) => {
                     match IshIshBlockchainEvent::try_from(&message.data)? {
-                        IshIshBlockchainEvent::NewBlockMined(msg) => {
-                            println!("Got new block: {msg}");
+                        IshIshBlockchainEvent::NewBlockMined(serialized) => {
+                            
+                            let deserialized: IshIshBlockchain = serde_json::from_str(&serialized)?;
+                            println!("Got new blockchain: {deserialized:?}, verifying");
+
+                            if deserialized.blocks.len() > my_blockchain.blocks.len()
+                            {
+                                println!("Received blockchain is heavier, verifying hashes");
+                                match deserialized.verify_chain() {
+                                    Ok(()) => {
+                                        println!("Verification passed, need to restart mining");
+                                        my_blockchain = deserialized;
+
+                                        /*  need to restart the mining thread */
+                                        control_tx.send(true).unwrap();
+
+                                        // Requestng mining new block
+                                        let mined_block = my_blockchain.blocks.last().unwrap();
+                                        let new_content = format!("Block number: {}", my_blockchain.blocks.len());
+
+                                        let mut next = IshIshBlock::linked_from_content(
+                                            new_content, 
+                                            mined_block.header.cur_hash
+                                        );
+
+                                        println!("Requesting mining of new block: {next:?}");
+                                        block_tx.send(next).await?;
+                                    }
+                                    Err(e) => {
+                                        println!("Blockchain verification failed, ignoring");
+                                    }
+                                }
+                            }
                         },
                         IshIshBlockchainEvent::SthElse((msg,re)) => {
                             println!("Something else: {msg} {re}");
