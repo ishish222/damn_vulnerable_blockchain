3359baeaff0801e3d84cf77684943e8855e04a27
ishish.io <ts@ishish.io>
2024-06-01 14:31:07 +0200

    Attempt to implement data availability layer

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,211 @@
-use futures::prelude::*;
-
+use async_std::println;
+use futures::stream::StreamExt;
 use libp2p::{
-    ping,
-    Multiaddr,
-    swarm::SwarmEvent
+    gossipsub, 
+    mdns, 
+    noise, 
+    swarm::NetworkBehaviour, 
+    swarm::SwarmEvent, 
+    tcp, 
+    yamux
 };
-use std::{
-    error::Error,
-    time::Duration
+use std::collections::hash_map::DefaultHasher;
+use std::error::Error;
+use std::hash::{
+    Hash, 
+    Hasher
+};
+use std::time::Duration;
+use tokio::{
+    io, 
+    io::AsyncBufReadExt, 
+    select
 };
 use tracing_subscriber::EnvFilter;
+/* 
+mod ishishnet {
 
+    use std::convert::TryFrom;
+    use std::error::Error;
+    use std::fmt::Display;
+    use std::fmt::Formatter;
+    use std::str;
+    use std::str::Utf8Error;
 
-#[async_std::main]
-async fn main() -> Result<(), Box<dyn Error>>{
-    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
-
-    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
-        .with_async_std()
-        .with_tcp(
-            libp2p::tcp::Config::default(),
-            libp2p::tls::Config::new,
-            libp2p::yamux::Config::default
-        )?
-        .with_behaviour(|_| ping::Behaviour::default())?
-        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30)))
-        .build();
-
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
-
-    if let Some(addr) = std::env::args().nth(1) {
-        let remote: Multiaddr = addr.parse()?;
-        swarm.dial(remote)?;
-        println!("Dialed {addr}")
+    #[derive(Debug)]
+    pub enum IshIshError {
+        ParseError,
+        EmptyMessage,
+        InvalidEvent
     }
 
-    loop {
-        match swarm.select_next_some().await {
-            SwarmEvent::NewListenAddr {address, ..} => println!("Listening on {address}"),
-            SwarmEvent::Behaviour(event) => println!("{event:?}"),
-            _ => {}
+    impl Display for IshIshError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> 
+        { 
+            write!(f, "Error");
+            Ok(())
         }
     }
 
-    Ok(())
+    impl Error for IshIshError {}
+
+    impl From<Utf8Error> for IshIshError {
+        fn from(_: Utf8Error) -> Self {
+            IshIshError::ParseError
+        }
+    }
+
+    pub enum IshIshBlockchainEvent {
+        NewBlockMined(String),
+        SthElse((String, String))
+    }
+
+    impl TryFrom<Vec<u8>> for IshIshBlockchainEvent {
+        type Error = IshIshError;
+
+        fn try_from(value: Vec<u8>) -> Result<Self, IshIshError> 
+        {
+            let value_str = str::from_utf8(&value)?;
+            match explode(value_str) {
+                Some((event, value_str)) => {
+                    match event {
+                        "NBM" => {
+                            return Ok(IshIshBlockchainEvent::NewBlockMined(value_str.into()))
+
+                        },
+                        _ => return Ok(IshIshBlockchainEvent::SthElse((event.into(), value_str.into())))
+                    }
+                }
+                None => return Err(IshIshError::EmptyMessage)
+            }
+        }
+    }
+
+    fn explode(message: &str) -> Option<(&str, &str)> {
+        message.chars().enumerate().find_map(|(i, c)| { 
+            if c == ' ' {
+                Some((&message[..i], &message[i+1..]))
+            }
+            else {
+                None
+            }
+        })
+    }
+    
+}
+
+use ishishnet::IshIshBlockchainEvent;
+*/
+// We create a custom network behaviour that combines Gossipsub and Mdns.
+#[derive(NetworkBehaviour)]
+struct IshIshClientBehavior {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    println!("Entry point");
+
+    let _ = tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init();
+
+    let mut swarm = libp2p::SwarmBuilder::with_new_identity()
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+            noise::Config::new,
+            yamux::Config::default,
+        )?
+        .with_behaviour(|key| {
+            // To content-address message, we can take the hash of message and use it as an ID.
+            let message_id_fn = |message: &gossipsub::Message| {
+                let mut s = DefaultHasher::new();
+                message.data.hash(&mut s);
+                gossipsub::MessageId::from(s.finish().to_string())
+            };
+
+            // Set a custom gossipsub configuration
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+                .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+                .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .build()
+                .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+            // build a gossipsub network behaviour
+            let gossipsub = gossipsub::Behaviour::new(
+                gossipsub::MessageAuthenticity::Signed(key.clone()),
+                gossipsub_config,
+            )?;
+
+            let mdns =
+                mdns::tokio::Behaviour::new(mdns::Config::default(), key.public().to_peer_id())?;
+            Ok(IshIshClientBehavior { gossipsub, mdns })
+        })?
+        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
+        .build();
+
+    // Create a Gossipsub topic
+    let topic = gossipsub::IdentTopic::new("test-net");
+    // subscribes to our topic
+    swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Listen on all interfaces and whatever port the OS assigns
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+
+    // Kick it off
+    loop {
+        select! {
+            Ok(Some(line)) = stdin.next_line() => {
+                if let Err(e) = swarm
+                    .behaviour_mut().gossipsub
+                    .publish(topic.clone(), line.as_bytes()) {
+                    println!("Publish error: {e:?}");
+                }
+            }
+            event = swarm.select_next_some() => match event {
+                SwarmEvent::Behaviour(IshIshClientBehaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discovered a new peer: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(IshIshClientBehaviorEvent::Mdns(mdns::Event::Expired(list))) => {
+                    for (peer_id, _multiaddr) in list {
+                        println!("mDNS discover peer has expired: {peer_id}");
+                        swarm.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                    }
+                },
+                SwarmEvent::Behaviour(IshIshClientBehaviorEvent::Gossipsub(gossipsub::Event::Message {
+                    propagation_source: peer_id,
+                    message_id: id,
+                    message,
+                })) => { println!(
+                    "Got message: '{}' with id: {id} from peer: {peer_id}",
+                    String::from_utf8_lossy(&message.data),                    
+                ); },
+                /*{
+                    match IshIshBlockchainEvent::try_from(message.data)? {
+                        IshIshBlockchainEvent::NewBlockMined(msg) => {
+                            println!("Got new block: {msg}");
+                        },
+                        IshIshBlockchainEvent::SthElse((msg,re)) => {
+                            println!("Something else: {msg} {re}");
+                        }
+                    }
+                },*/
+                SwarmEvent::NewListenAddr { address, .. } => {
+                    println!("Local node is listening on {address}");
+                }
+                _ => {}
+            }
+        }
+    }
 }
